6617745294016915d1262fb9ac20a290c867af04
Lawal Ayobami <bamiayo90@gmail.com>
2026-03-14 22:49:58 +0100

    cron job

--- server.js
+++ server.js
@@ -1,11 +1,29 @@
 require("dotenv").config();
+const cron = require("node-cron");
 const logger = require("./util/logger");
 const app = require("./app");
+const { Company } = require("./models");
 require("./database/create_table").init();
 
 const PORT = process.env.PORT || 3333;
 const server = app.listen(PORT, () => {
   console.log("server listening on port " + PORT);
+});
+
+async function queryDatabse() {
+  await Company.findOne({});
+}
+cron.schedule("0 0 * * *", () => {
+  queryDatabse();
+});
+
+cron.schedule("0 * * * *", () => {
+  fetch("https://shift-yuw6.onrender.com/test")
+    .then((response) => response.json())
+    .then((res) => {})
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 process.on("unhandledRejection", (err) => {
