2b11bad39e1dd33b578162fe70a1abfdd7366766
Lawal Ayobami <bamiayo90@gmail.com>
2026-04-22 18:35:32 +0100

    relax cron job

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
-const cron = require("node-cron");
+// const cron = require("node-cron");
 const logger = require("./util/logger");
 const app = require("./app");
-const { Company } = require("./models");
+// const { Company } = require("./models");
 require("./database/create_table").init();
 
 const PORT = process.env.PORT || 3333;
@@ -10,23 +10,23 @@ const server = app.listen(PORT, () => {
   console.log("server listening on port " + PORT);
 });
 
-async function queryDatabse() {
-  await Company.findOne({});
-}
-cron.schedule("0 0 * * *", () => {
-  queryDatabse();
-});
+// async function queryDatabse() {
+//   await Company.findOne({});
+// }
+// cron.schedule("0 0 * * *", () => {
+//   queryDatabse();
+// });
 
-cron.schedule("*/14 * * * *", () => {
-  fetch("https://shift-yuw6.onrender.com/test")
-    .then((response) => response.json())
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+// cron.schedule("*/14 * * * *", () => {
+//   fetch("https://shift-yuw6.onrender.com/test")
+//     .then((response) => response.json())
+//     .then((res) => {
+//       console.log(res);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// });
 
 process.on("unhandledRejection", (err) => {
   logger.error("Unhandled Rejection! Shutting down ❌");
